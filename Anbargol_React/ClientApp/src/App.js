a377a36e4d218451d934011b9935a5fcd3f15362
IT2 <IT2@IT2>
2020-08-15 14:50:51 +0430

    navbar implemented

--- Anbargol_React/ClientApp/src/App.js
+++ Anbargol_React/ClientApp/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import classes from './Assets/Css/app.module.css';
+import Navbar from './Components/Header/Navbar/Navbar';
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
+        <Navbar />
         123
       </div>
     );
