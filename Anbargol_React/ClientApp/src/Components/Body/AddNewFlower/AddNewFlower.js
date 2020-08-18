bc69045bf8459e9cc1901a12f62f938342c8c685
Omid <Omid@DESKTOP-LJM88T4>
2020-08-18 23:30:21 +0430

    try to find out how redux work

--- Anbargol_React/ClientApp/src/Components/Body/AddNewFlower/AddNewFlower.js
+++ Anbargol_React/ClientApp/src/Components/Body/AddNewFlower/AddNewFlower.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class AddNewFlower extends Component {
     render() {
+        console.log(this.props)
         return (
             123
         )
     }
 }
+const mapStateToProps = state => {
+    console.log(state);
+    return {
+        omid: state.result
+    }
+}
+const mapDispatchToProps = dispatch => {
+    console.log(dispatch)
+    return {
+        omidi: () => dispatch({ type: 'F' })
+    }
+}
 
-export default AddNewFlower;
+export default connect(mapStateToProps, mapDispatchToProps)(AddNewFlower);
