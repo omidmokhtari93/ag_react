2469cfbaf32a0ac9f3f5c46700c7640dadd45ab5
Omid <Omid@DESKTOP-LJM88T4>
2020-08-25 23:36:06 +0430

    minor change

--- Anbargol_React/ClientApp/src/App.js
+++ Anbargol_React/ClientApp/src/App.js
@@ -3,7 +3,6 @@ import Navbar from './Components/Header/Navbar/TopNavBar/TopNavbar';
 import Wrapper from './Shared/Wrapper/Wrapper';
 import AddNewFlower from './Components/Body/AddNewFlower/AddNewFlower';
 import AddFlowerForms from './Components/Body/AddFlowerForms/AddFlowerForms';
-
 import { Route } from 'react-router-dom';
 import ErrorPage from './Shared/ErrorPage/ErrorPage';
 
@@ -15,7 +14,7 @@ export default class App extends Component {
         <div className="container sans p-4 border">
           <Route path="/addnew" exact render={() => <AddNewFlower />} />
           <Route path="/addforms" exact render={() => <AddFlowerForms />} />
-          <Route path="/error" exact render={() => <ErrorPage/>} />
+          <Route path ="/" exact render={() => <ErrorPage/>} />
         </div>
       </Wrapper>
     );
