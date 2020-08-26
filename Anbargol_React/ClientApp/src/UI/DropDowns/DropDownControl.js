d21f4ed9c51b64cdc176f611c56ddd63e804e306
IT2 <IT2@IT2>
2020-08-26 14:45:59 +0430

    minor change

--- Anbargol_React/ClientApp/src/UI/DropDowns/DropDownControl.js
+++ Anbargol_React/ClientApp/src/UI/DropDowns/DropDownControl.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import Wrapper from '../../Shared/Wrapper/Wrapper';
 import Input from '../Inputs/Input';
+import './DropDown.module.css';
 
 class DropDownControl extends Component {
     state = {
         options: [{ name: 'فرم 1', value: 9865 }],
+        value: '',
+        type: "select",
+        required: true,
+        touched: false,
+        name: "selectform",
+        label: "نام آیتم",
+        handleChange: (name, value) => this.handleChange(name, value),
     }
 
     handleChange = (name, value) => {
+        let updatedState = { ...this.state };
+        updatedState.value = value;
+        updatedState.touched = true;
         console.log(name, value)
+        this.setState({ ...updatedState })
     }
-    
+
     render() {
         return (
             <Wrapper>
-                <Input type="select"
-                    options={this.state.options}
-                    required
-                    touched
-                    name="selectform"
-                    handleChange={this.handleChange} />
+                <div className="drop-down-control">
+                    <div style={{ marginLeft: '30%' }}>
+                        <Input {...this.state} />
+                    </div>
+                    <button>{'>'}</button>
+                    <button>{'<'}</button>
+                </div>
             </Wrapper>
         )
     }
