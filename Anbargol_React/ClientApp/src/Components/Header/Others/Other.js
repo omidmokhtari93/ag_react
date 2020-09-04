bba7ed3e1fb5089047ed9ffe963d6baa296f65fc
Omid <Omid@DESKTOP-LJM88T4>
2020-09-04 22:17:10 +0430

    new concept test

--- Anbargol_React/ClientApp/src/Components/Header/Others/Other.js
+++ Anbargol_React/ClientApp/src/Components/Header/Others/Other.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useHistory, useParams } from "react-router";
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
+import Wrapper from '../../../Shared/Wrapper/Wrapper';
 
 const mapStateToProps = state => {
     return {
@@ -11,16 +12,18 @@ const mapStateToProps = state => {
 
 export const MyProducts = connect(mapStateToProps)(withRouter((props) => {
 
-    
+    const [timer, incTimer] = useState(0);
 
     const [counter, incCounter] = useState(0);
 
     useEffect(() => {
-        document.title = `You clicked ${counter} times`;
-        console.log(props)
-    }, [counter]);
+        console.log('USE EFFECT')
+    },[counter]);
 
     return (
-        <div onClick={() => incCounter(counter - 1)}>{props.some} {counter}</div>
+        <Wrapper>
+            <div onClick={() => incCounter(counter - 1)}>{props.some} {counter}</div>
+            <div onClick={() => incTimer(timer + 10)}>{props.some} {timer}</div>
+        </Wrapper>
     )
 }))
