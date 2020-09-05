788cdd3d07166ae6a97ecf58619572c857a500c9
IT2 <IT2@IT2>
2020-09-05 09:54:09 +0430

    error boundary added

--- Anbargol_React/ClientApp/src/App.js
+++ Anbargol_React/ClientApp/src/App.js
@@ -7,6 +7,7 @@ import AddFlowerItems from './Components/Body/AddFlowerItems/AddFlowerItems';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import ErrorPage from './Shared/ErrorPage/ErrorPage';
 import AddFlowerOrders from './Components/Body/AddFlowerOrders/AddFlowerOrder';
+import ErrorBoundary from './Shared/ErrorBoundary';
 
 export default class App extends Component {
   render() {
@@ -14,13 +15,15 @@ export default class App extends Component {
       <Wrapper>
         <Navbar />
         <div className="container sans p-4 border mt-3">
-          <Switch>
-            <Route path="/addnew" render={() => <AddNewFlower />} />
-            <Route path="/addforms/:flowerId" exact render={() => <AddFlowerForms />} />
-            <Route path="/additems/:flowerId" exact render={() => <AddFlowerItems />} />
-            <Route path="/addorders/:flowerId" exact render={() => <AddFlowerOrders />} />
-            <Route render={() => <ErrorPage />} />
-          </Switch>
+          <ErrorBoundary>
+            <Switch>
+              <Route path="/addnew" render={() => <AddNewFlower />} />
+              <Route path="/addforms/:flowerId" exact render={() => <AddFlowerForms />} />
+              <Route path="/additems/:flowerId" exact render={() => <AddFlowerItems />} />
+              <Route path="/addorders/:flowerId" exact render={() => <AddFlowerOrders />} />
+              <Route render={() => <ErrorPage />} />
+            </Switch>
+          </ErrorBoundary>
         </div>
       </Wrapper>
     );
